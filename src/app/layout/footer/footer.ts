5ee14305b6793f84da9d541fb1c019fb524ead5d
6-LL <lorenzanilautaro+github@gmail.com>
2026-03-23 18:16:29 -0300

    footer, header and home page layouts added

---
+++ src/app/layout/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'footer',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+
+}
