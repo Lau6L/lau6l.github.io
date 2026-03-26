46c00eef00cccf916008cc737c437364d6efdb20
6-LL <lorenzanilautaro+github@gmail.com>
2026-03-26 14:02:56 -0300

    component routes & contact component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './layout/home/home';
+import { Projects } from './layout/projects/projects';
+import { Contact } from './layout/contact/contact';
 
 export const routes: Routes = [
     {path: 'home', component: Home},
+    {path: 'projects', component: Projects},
+    {path: 'contact', component: Contact},
+
     {path: '**', redirectTo: '/home'}
 ];
