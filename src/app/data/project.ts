7fef970da2c9916f60b9c1021a150829d30c9ddc
6-LL <lorenzanilautaro+github@gmail.com>
2026-03-26 12:00:38 -0300

    project, project type and link interfaces added

---
+++ src/app/data/project.ts
@@ -0,0 +1,18 @@
+export interface Project {
+    name: string,
+    type: ProjectType,
+    description: string,
+    source: string,
+    completed: boolean,
+    links?: Link[]
+}
+
+export enum ProjectType {
+    MinecraftMod = "minecraft_mod"
+}
+
+export interface Link {
+    url: string,
+    provider: string,
+    image: string
+}
