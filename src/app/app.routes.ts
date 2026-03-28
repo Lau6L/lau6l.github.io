7acf5ac14ab01968eef1f4aec9b16cb67ff1e457
6-LL <lorenzanilautaro+github@gmail.com>
2026-03-28 19:18:07 -0300

    about me layout added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './layout/home/home';
 import { Projects } from './layout/projects/projects';
 import { Contact } from './layout/contact/contact';
+import { AboutMe } from './layout/about-me/about-me';
 
 export const routes: Routes = [
     {path: 'home', component: Home},
+    {path: 'about-me', component: AboutMe},
     {path: 'projects', component: Projects},
     {path: 'contact', component: Contact},
 
