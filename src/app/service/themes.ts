18d44f2b5fafe63f198ffa245706cdb6ad8c990c
6-LL <lorenzanilautaro+github@gmail.com>
2026-04-04 21:18:52 -0300

    theme logic storage added

--- src/app/service/themes.ts
+++ src/app/service/themes.ts
@@ -1,21 +1,51 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Themes {
-  public toggleTheme() {
-    const dark = "dark";
-    const bodyClasses = document.body.classList;
+  private readonly THEME_KEY = 'theme';
+  private isBrowser: boolean;
 
-    if (bodyClasses.contains(dark)) {
-      bodyClasses.remove(dark)
-    } else {
-      bodyClasses.add(dark)
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+    if (this.isBrowser) {
+      this.initTheme();
     }
   }
 
-  public getTheme() {
-    return document.body.classList.contains("dark") ? "dark" : "light";
+  private initTheme() {
+    const saved = localStorage.getItem(this.THEME_KEY);
+
+    if (saved) {
+      this.applyTheme(saved);
+    } else {
+      const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      const theme = prefersDarkTheme ? 'dark' : 'light';
+
+      this.applyTheme(theme);
+    }
+  }
+
+  private applyTheme(theme: string) {
+    const classes = document.body.classList;
+
+    // 'light' class has no actual effect, it's here for coherence
+    classes.remove('light', 'dark');
+    classes.add(theme);
+  }
+
+  public toggleTheme() {
+    if (!this.isBrowser) return;
+    const theme = this.getTheme() === 'dark' ? 'light' : 'dark';
+
+    this.applyTheme(theme);
+    localStorage.setItem(this.THEME_KEY, theme);
+  }
+
+  public getTheme(): string {
+    if (!this.isBrowser) return 'light';
+    return document.body.classList.contains('dark') ? 'dark' : 'light';
   }
 }
