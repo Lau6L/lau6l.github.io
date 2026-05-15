7415f2012590194e7bbc8b060c9d88a1f8291dec
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-15 18:01:18 -0300

    Skills component added to routes, header, and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Home } from './layout/home/home';
 import { Projects } from './layout/projects/projects';
 import { Contact } from './layout/contact/contact';
 import { AboutMe } from './layout/about-me/about-me';
+import { Skills } from './layout/skills/skills';
 
 export const routes: Routes = [
     {path: 'home', component: Home},
     {path: 'about-me', component: AboutMe},
     {path: 'projects', component: Projects},
     {path: 'contact', component: Contact},
+    {path: 'skills', component: Skills},
 
     {path: '**', redirectTo: '/home'}
 ];
